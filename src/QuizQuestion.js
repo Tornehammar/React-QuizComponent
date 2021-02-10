aa21074e4f8358b56d8c0fa682c374473c14ce6c
Tornehammar <bardia.tornehammar@gmail.com>
2021-02-10 14:54:01 +0100

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,7 @@ render(){
   return (
     <main>
        <section>
-         <p> {this.props.quiz_question.instruction_text } </p>
+         <p> {this.props.quiz_question.instruction_text} </p>
        </section>
        <section className="buttons">
          <ul>
